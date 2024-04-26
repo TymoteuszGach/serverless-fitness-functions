fa519869c19e03f7a0138b3e5342a9a46d9fb029
Tymoteusz Gach <tymoteusz.gach@merapar.com>
2024-04-26 15:42:52 +0200

    fix failing test

--- src/application/domain/model/email.ts
+++ src/application/domain/model/email.ts
@@ -1,5 +1,3 @@
-import { badRequestResult } from '@adapters/primary/lambda/responses';
-
 export const validateEmail = (value: string): boolean => {
   return (
     String(value)
@@ -8,8 +6,4 @@ export const validateEmail = (value: string): boolean => {
         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
       ) !== null
   );
-};
-
-export const a = (): void => {
-  console.log(badRequestResult);
 };
